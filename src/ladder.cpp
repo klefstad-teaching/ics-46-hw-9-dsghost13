4c7f445d7f09642110146729af642374aece8495
Peter Tandat Le <petertl2@uci.edu>
2025-03-15 01:35:16 +0000

    s

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -65,7 +65,7 @@ void load_words(set<string>& word_list, const string& file_name) {
 
 void print_word_ladder(const vector<string>& ladder) {
     for (size_t i = 0; i < ladder.size(); ++i) {
-        cout << ladder[i] < " ";
+        cout << ladder[i] << " ";
     }
     if (ladder.size() == 0) {
         cout << "No word ladder found.";
